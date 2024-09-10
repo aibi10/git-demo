9fedff4169c0d3d96ff7af1faea8b714b8f75b9f
abhishek-eand <156221560+abhishek-eand@users.noreply.github.com>
2024-09-11 00:38:16 +0400

    third commit

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-def print_name(name):
-    print(name)
+from addsub import add, sub
 
-name = "srk"
-print_name(name)
+def main():
+    print("Welcome to the calculator app")
+
+    print("""
+    1. Addition
+    2. Subtraction
+    """)
+
+    choice = int(input("enter your choice\n"))
+
+    a = int(input("enter the first number\n"))
+    b = int(input("enter the second number\n"))
+
+    if choice == 1:
+        result = add(a, b)
+    elif choice == 2:
+        result = sub(a, b)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
